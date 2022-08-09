5a7784c088d183495297ecb16b9853a44efe5db0
Farai Major <faraimutukumira@gmail.com>
2022-08-09 13:23:14 -0400

    connected to mongoDB and saved data

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react'
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+
 import useStyles from './styles';
+import { createPost } from '../../actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({ creator: '', message: '', tags: '', selectedFile: '' })
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
@@ -36,4 +42,4 @@ const Form = () => {
     );
 }
 
-export default Form;
+export default Form; 
